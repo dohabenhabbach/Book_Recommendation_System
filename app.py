2765533087af9baaadef2a8032013786392fa16f
dohabenhabbach <dohaben0601@gmail.com>
2023-09-22 14:30:12 +0100

    last commit

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def index():
                            author=list(popular_df['Book-Author'].values),
                            image=list(popular_df['Image-URL-M'].values),
                            votes=list(popular_df['num_ratings'].values),
-                           rating=list(popular_df['avg_rating'].values)
+                           rating=list(popular_df['avg_ratings'].values)
                            )
 
 @app.route('/recommend')
